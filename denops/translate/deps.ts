68828b322051628a159903ef5fc296fe9cc544ea
GitHub <noreply@github.com>
2021-09-24 00:20:37 +0000

    chore(deps): update deno dependencies

--- denops/translate/deps.ts
+++ denops/translate/deps.ts
@@ -1,11 +1,11 @@
-export type { Denops } from "https://deno.land/x/denops_std@v1.9.0/mod.ts";
-export { execute } from "https://deno.land/x/denops_std@v1.9.0/helper/mod.ts";
-export * as vars from "https://deno.land/x/denops_std@v1.9.0/variable/mod.ts";
-export * as autocmd from "https://deno.land/x/denops_std@v1.9.0/autocmd/mod.ts";
+export type { Denops } from "https://deno.land/x/denops_std@v1.11.3/mod.ts";
+export { execute } from "https://deno.land/x/denops_std@v1.11.3/helper/mod.ts";
+export * as vars from "https://deno.land/x/denops_std@v1.11.3/variable/mod.ts";
+export * as autocmd from "https://deno.land/x/denops_std@v1.11.3/autocmd/mod.ts";
 export {
   ensureArray,
   ensureNumber,
   ensureString,
   isString,
-} from "https://deno.land/x/unknownutil@v1.1.0/mod.ts";
+} from "https://deno.land/x/unknownutil@v1.1.1/mod.ts";
 export * as popup from "https://raw.githubusercontent.com/Omochice/denops-popup/vim-popup-option/mod.ts";
