77f6e6486f909d5e31d1ea0d97be5b9fcb656a6a
GitHub <noreply@github.com>
2021-10-15 00:22:07 +0000

    :package: update deno dependencies

--- denops/translate/deps.ts
+++ denops/translate/deps.ts
@@ -1,12 +1,12 @@
-export type { Denops } from "https://deno.land/x/denops_std@v2.0.1/mod.ts";
-export { execute } from "https://deno.land/x/denops_std@v2.0.1/helper/mod.ts";
-export * as vars from "https://deno.land/x/denops_std@v2.0.1/variable/mod.ts";
-export * as autocmd from "https://deno.land/x/denops_std@v2.0.1/autocmd/mod.ts";
+export type { Denops } from "https://deno.land/x/denops_std@v2.1.2/mod.ts";
+export { execute } from "https://deno.land/x/denops_std@v2.1.2/helper/mod.ts";
+export * as vars from "https://deno.land/x/denops_std@v2.1.2/variable/mod.ts";
+export * as autocmd from "https://deno.land/x/denops_std@v2.1.2/autocmd/mod.ts";
 export {
   ensureArray,
   ensureNumber,
   ensureString,
   isString,
-} from "https://deno.land/x/unknownutil@v1.1.3/mod.ts";
+} from "https://deno.land/x/unknownutil@v1.1.4/mod.ts";
 export { openPopup } from "https://pax.deno.dev/Omochice/dps-popup-toy/mod.ts"
 
--- test/dev_deps.ts
+++ test/dev_deps.ts
@@ -4,4 +4,4 @@ export {
   assertStrictEquals,
   assertStringIncludes,
   assertThrowsAsync,
-} from "https://deno.land/std@0.110.0/testing/asserts.ts";
+} from "https://deno.land/std@0.111.0/testing/asserts.ts";
