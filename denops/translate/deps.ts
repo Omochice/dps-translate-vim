f0968c48595036a87aa1cc5e465d58870b8339cf
mochi-w650z <mochice.mls.ntl@gmail.com>
2022-02-16 03:53:23 +0900

    :+1: Use Intl.Segmenter

--- denops/translate/deps.ts
+++ denops/translate/deps.ts
@@ -1,5 +1,6 @@
 export type { Denops } from "https://deno.land/x/denops_std@v3.0.0/mod.ts";
 export { execute } from "https://deno.land/x/denops_std@v3.0.0/helper/mod.ts";
+export * as fn from "https://deno.land/x/denops_std@v3.0.0/function/mod.ts";
 export * as vars from "https://deno.land/x/denops_std@v3.0.0/variable/mod.ts";
 export * as autocmd from "https://deno.land/x/denops_std@v3.0.0/autocmd/mod.ts";
 export {
@@ -8,5 +9,4 @@ export {
   ensureString,
   isString,
 } from "https://deno.land/x/unknownutil@v1.1.4/mod.ts";
-export { openPopup } from "https://pax.deno.dev/Omochice/dps-popup-toy/mod.ts"
-
+export { openPopup } from "https://pax.deno.dev/Omochice/dps-popup-toy/mod.ts";
--- denops/translate/translate.ts
+++ denops/translate/translate.ts
@@ -1,37 +1,41 @@
-const endpoint =
-  "https://script.google.com/macros/s/AKfycbzdOBxUB9-PeT86IhOJO7oTbjEjJAf8ECUfrqW06eKLQTy8xdaLtUBmexx94Jl3cLNb/exec";
-
-interface translateResult {
+interface TranslateResult {
   code: number;
   text: string;
 }
 
 export async function translate(
-  rowStr: string | string[],
+  text: string[],
   sourceLanguage: string,
   targetLanguage: string,
-): Promise<translateResult> {
-  const text = typeof rowStr === "string" ? rowStr : rowStr.join("\n");
-
-  const url = endpoint + "?" + new URLSearchParams({
-    text: text,
+): Promise<TranslateResult> {
+  const endpoint =
+    "https://script.google.com/macros/s/AKfycbzdOBxUB9-PeT86IhOJO7oTbjEjJAf8ECUfrqW06eKLQTy8xdaLtUBmexx94Jl3cLNb/exec";
+  const body = new URLSearchParams({
+    text: text.join("\n"),
     source: sourceLanguage,
     target: targetLanguage,
   });
 
-  const res = await fetch(url);
+  const res = await fetch(endpoint + "?" + body, {
+    method: "GET",
+  }); // `fetch` with GET method cannot handle `body` parameter
+
   if (res.status != 200) {
     throw new Error(`Invalid status [${res.status}]`);
   } else {
     try {
-      const json = await res.json();
+      const json: TranslateResult = await res.json();
       if (json.code != 200) {
         throw new Error(`Invalid status [${json.code}]`);
       }
-      return json
+      return json;
     } catch (e) {
       // json decode error or not 200
       throw e;
     }
   }
+}
+
+export function extractTranslatedText(response: TranslateResult): string {
+  return response.text;
 }
