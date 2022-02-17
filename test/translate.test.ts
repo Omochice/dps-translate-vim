b854b704d47e74d6eec2fbf7a8ad26e8ba04cb4f
mochi-w650z <mochice.mls.ntl@gmail.com>
2022-02-18 08:06:58 +0900

    :hammer: Fix test

--- test/translate.test.ts
+++ test/translate.test.ts
@@ -2,35 +2,41 @@ import { translate } from "../denops/translate/translate.ts";
 import { assert, assertEquals, assertRejects } from "./dev_deps.ts";
 
 const MOCK_404 = async (): Promise<Response> => {
-  return await new Response(
-    null,
-    {
-      status: 404,
-      statusText: "404 test",
-      headers: { "title": "test", "text": "test" },
-    },
+  return await Promise.resolve(
+    new Response(
+      null,
+      {
+        status: 404,
+        statusText: "404 test",
+        headers: { "title": "test", "text": "test" },
+      },
+    ),
   );
 };
 
 const MOCK_200 = async (): Promise<Response> => {
-  return await new Response(
-    '{"code": 200, "text": "test"}',
-    {
-      status: 200,
-      statusText: "200 test",
-      headers: { "content-type": "application/json" },
-    },
+  return await Promise.resolve(
+    new Response(
+      '{"code": 200, "text": "test"}',
+      {
+        status: 200,
+        statusText: "200 test",
+        headers: { "content-type": "application/json" },
+      },
+    ),
   );
 };
 
 const MOCK_INVALID_JSON = async (): Promise<Response> => {
-  return await new Response(
-    '{"this is invalid json formated text}',
-    {
-      status: 200,
-      statusText: "200 test",
-      headers: { "content-type": "application/json" },
-    },
+  return await Promise.resolve(
+    new Response(
+      '{"this is invalid json formated text}',
+      {
+        status: 200,
+        statusText: "200 test",
+        headers: { "content-type": "application/json" },
+      },
+    ),
   );
 };
 
@@ -47,12 +53,17 @@ Deno.test({
 Deno.test({
   name:
     "If returned status code is 200, the function should return valid result.",
-  fn: async () => {
+  fn: async (t) => {
     self.fetch = MOCK_200;
     const actual = await translate(["test string"], "EN", "JA");
-    assert("code" in actual, "result should have 'code' property");
-    assert("text" in actual, "result should have 'text' property");
-    assertEquals(actual.code, 200, "result.code should be 200");
+    await t.step("Has result expected fields?", () => {
+      assert("code" in actual, "result should have 'code' property");
+      assert("text" in actual, "result should have 'text' property");
+    });
+    await t.step(
+      "Is code 200",
+      () => assertEquals(actual.code, 200, "result.code should be 200"),
+    );
   },
 });
 
