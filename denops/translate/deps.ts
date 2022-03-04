a888af90fd0b05b36074967f430d7975bb6c65e4
mochi-w650z <mochice.mls.ntl@gmail.com>
2022-03-05 04:45:26 +0900

    :package: Update deno dependencies

--- denops/translate/deps.ts
+++ denops/translate/deps.ts
@@ -1,12 +1,12 @@
-export type { Denops } from "https://deno.land/x/denops_std@v3.0.0/mod.ts";
-export { execute } from "https://deno.land/x/denops_std@v3.0.0/helper/mod.ts";
-export * as fn from "https://deno.land/x/denops_std@v3.0.0/function/mod.ts";
-export * as vars from "https://deno.land/x/denops_std@v3.0.0/variable/mod.ts";
-export * as autocmd from "https://deno.land/x/denops_std@v3.0.0/autocmd/mod.ts";
+export type { Denops } from "https://deno.land/x/denops_std@v3.1.4/mod.ts";
+export { execute } from "https://deno.land/x/denops_std@v3.1.4/helper/mod.ts";
+export * as fn from "https://deno.land/x/denops_std@v3.1.4/function/mod.ts";
+export * as vars from "https://deno.land/x/denops_std@v3.1.4/variable/mod.ts";
+export * as autocmd from "https://deno.land/x/denops_std@v3.1.4/autocmd/mod.ts";
 export {
   ensureArray,
   ensureNumber,
   ensureString,
   isString,
-} from "https://deno.land/x/unknownutil@v1.1.4/mod.ts";
+} from "https://deno.land/x/unknownutil@v2.0.0/mod.ts";
 export { openPopup } from "https://pax.deno.dev/Omochice/dps-popup-toy/mod.ts";
