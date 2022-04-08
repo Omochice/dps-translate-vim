ae8990c24539cd26db11f2416ecca91afa04d31e
GitHub <noreply@github.com>
2022-04-08 00:37:00 +0000

    :package: update deno dependencies

--- denops/translate/deps.ts
+++ denops/translate/deps.ts
@@ -1,8 +1,8 @@
-export type { Denops } from "https://deno.land/x/denops_std@v3.1.4/mod.ts";
-export { execute } from "https://deno.land/x/denops_std@v3.1.4/helper/mod.ts";
-export * as fn from "https://deno.land/x/denops_std@v3.1.4/function/mod.ts";
-export * as vars from "https://deno.land/x/denops_std@v3.1.4/variable/mod.ts";
-export * as autocmd from "https://deno.land/x/denops_std@v3.1.4/autocmd/mod.ts";
+export type { Denops } from "https://deno.land/x/denops_std@v3.3.0/mod.ts";
+export { execute } from "https://deno.land/x/denops_std@v3.3.0/helper/mod.ts";
+export * as fn from "https://deno.land/x/denops_std@v3.3.0/function/mod.ts";
+export * as vars from "https://deno.land/x/denops_std@v3.3.0/variable/mod.ts";
+export * as autocmd from "https://deno.land/x/denops_std@v3.3.0/autocmd/mod.ts";
 export {
   assertArray,
   assertNumber,
