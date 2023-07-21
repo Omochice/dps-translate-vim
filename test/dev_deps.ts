ec497a3841d0331ec26eab93479e0dce4525aa0d
GitHub <noreply@github.com>
2023-07-21 00:40:16 +0000

    :package: update deno dependencies

--- test/dev_deps.ts
+++ test/dev_deps.ts
@@ -4,4 +4,4 @@ export {
   assertRejects,
   assertStrictEquals,
   assertStringIncludes,
-} from "https://deno.land/std@0.174.0/testing/asserts.ts";
+} from "https://deno.land/std@0.195.0/testing/asserts.ts";
