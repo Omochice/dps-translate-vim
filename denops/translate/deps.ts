523e6479121b29fdce55f40d326b7fcc39a41d4e
GitHub <noreply@github.com>
2024-03-01 00:37:00 +0000

    :package: update deno dependencies

--- denops/translate/deps.ts
+++ denops/translate/deps.ts
@@ -1,8 +1,8 @@
-export type { Denops } from "https://deno.land/x/denops_std@v5.3.0/mod.ts";
-export { execute } from "https://deno.land/x/denops_std@v5.3.0/helper/mod.ts";
-export * as fn from "https://deno.land/x/denops_std@v5.3.0/function/mod.ts";
-export * as vars from "https://deno.land/x/denops_std@v5.3.0/variable/mod.ts";
-export * as autocmd from "https://deno.land/x/denops_std@v5.3.0/autocmd/mod.ts";
+export type { Denops } from "https://deno.land/x/denops_std@v6.2.0/mod.ts";
+export { execute } from "https://deno.land/x/denops_std@v6.2.0/helper/mod.ts";
+export * as fn from "https://deno.land/x/denops_std@v6.2.0/function/mod.ts";
+export * as vars from "https://deno.land/x/denops_std@v6.2.0/variable/mod.ts";
+export * as autocmd from "https://deno.land/x/denops_std@v6.2.0/autocmd/mod.ts";
 export {
   assert,
   ensure,
